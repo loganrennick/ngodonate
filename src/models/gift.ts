53c413f4305a2f504150c5d344a027364341d235
Logan <loganrennick@gmail.com>
2021-06-04 17:26:53 -0400

    added additional models

---
+++ src/models/gift.ts
@@ -0,0 +1,5 @@
+export class Gift {
+    public id: number = 0;
+    public price: number = 0;
+    public monthly: boolean = false;
+}
---
+++ src/models/personal-info.ts
@@ -0,0 +1,12 @@
+export class PersonalInfo {
+    public id: number = 0;
+    public firstName: string = "";
+    public lastName: string = "";
+    public phoneNum: number = 0;
+    public email: string = "";
+    public address: string = "";
+    public city: string = "";
+    public state: string = "";
+    public zip: number = 0;
+    public country: string = "";
+}
