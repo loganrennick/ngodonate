3ad553936fa7a332def6d0f1bd41668093be3cb6
Logan <loganrennick@gmail.com>
2021-06-08 16:22:19 -0400

    linked donation start component to donation type service

--- src/app/donation-start/donation-start.component.ts
+++ src/app/donation-start/donation-start.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DonationType } from 'src/models/donation-type';
+import { DonationTypeService } from '../services/donation-type.service';
 
 @Component({
   selector: 'app-donation-start',
@@ -9,19 +10,20 @@ import { DonationType } from 'src/models/donation-type';
 })
 export class DonationStartComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, private ngoService: DonationTypeService) { }
+
+  public groups: any;
+  errorMsg: any;
 
   ngOnInit(): void {
+    this.ngoService.getDonationTypes().subscribe(
+      (data) => {
+        this.groups = data;
+        console.log(data);
+      },
+      (error) => this.errorMsg = error
+    )
   }
-
-  // public groups: DonationType[] = [
-  //   { id: 1, Name: "Step Up for Students" },
-  //   { id: 2, Name: "Feed the Children" },
-  //   { id: 3, Name: "American Heart Association" },
-  //   { id: 4, Name: "Electronic Frontier Foundation" },
-  //   { id: 5, Name: "Room to Read" }
-  // ];
-  public groups: any;
 
   onClick() {
     this.router.navigate(['/personal-information/']);
