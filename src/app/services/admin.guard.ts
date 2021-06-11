a708f844889b03de4ae99a065f719fa78737e2f4
Logan <loganrennick@gmail.com>
2021-06-11 09:49:42 -0400

    added route guards

--- src/app/services/admin.guard.ts
+++ src/app/services/admin.guard.ts
@@ -6,16 +6,15 @@ import { LoginService } from './login.service';
 @Injectable({
   providedIn: 'root'
 })
-export class CanactivateGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
   constructor(private router: Router, public authService: LoginService) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!this.authService.isUserLoggedIn()) {
+    if (!this.authService.isUserLoggedIn() || !this.authService.isUserAdmin()) {
       alert('You are not allowed to view this page. You are being redirected to the login page');
       this.router.navigate([''], { queryParams: { retUrl: route.url } });
       return false;
-
     }
     return true;
   }
